8eeaefbff14b14210caebb597811f58ce53915ea
LMDURAN09 <lauris_mauris212@hotmail.com>
2025-03-01 20:00:44 -0500

    MAPAV4

--- Trabajo01/program.js
+++ Trabajo01/program.js
@@ -3,3 +3,17 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+async function loadpolgom() 
+try{
+    let responde = await fetch ("Piamonte.geojson");
+    let data = await responde.json();
+    l.geoJSON(data,
+        {
+            style: {Color: "Blue"},
+        }
+    ).addTo(Mapas);
+} 
+    catch(error){
+        console.error("Cant load")
+    }
