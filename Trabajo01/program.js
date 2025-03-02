67c58f7bd824931fbe701b4838ff265045d19c9e
LMDURAN09 <lauris_mauris212@hotmail.com>
2025-03-01 19:29:22 -0500

    Mapas vr3

--- Trabajo01/program.js
+++ Trabajo01/program.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([4.608199262243065, -74.19120122030391], 20;
+var map = L.map('map').setView([4,608215, -74,191166], 20);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
