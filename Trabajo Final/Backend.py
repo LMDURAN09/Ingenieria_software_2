22d787a50cedaed5066333608a5123086d5f271a
LMDURAN09 <lauris_mauris212@hotmail.com>
2025-03-28 23:17:27 -0500

    Update Backend.py

--- Trabajo Final/Backend.py
+++ Trabajo Final/Backend.py
@@ -1,9 +1,10 @@
-from flask import Flask, jsonify
-import mysql.connector
-from flask_cors import CORS  # Permite que el frontend acceda a la API
+from flask import Flask, jsonify # Contruccion de ApIS en Phyton, Convierte los datos en formato JSON para enviarlos al cliente.
+import mysql.connector #Conexion a la base de datos 
+from flask_cors import CORS  # Permite que el frontend acceda a la API, 
 
-app = Flask(__name__)
-CORS(app)  # Habilita CORS para permitir solicitudes desde el navegador
+app = Flask(__name__) # Inicia la aplicacion 
+CORS(app)  # Habilita CORS para permitir solicitudes desde el navegador,
+#Permite solicitudes desde navegadores que corren en dominios distintos al del backend.
 
 port = 3000
 
@@ -16,11 +17,11 @@ db_config = {
 }
 
 # Ruta para obtener dispositivos con sus ubicaciones y mediciones
-@app.route('/api/dispositivos', methods=['GET'])
-def get_dispositivos():
+@app.route('/api/dispositivos', methods=['GET']) #Define una URL (/api/dispositivos) donde se podrá hacer una solicitud GET para obtener los datos.
+def get_dispositivos(): #Función que ejecutará la consulta a la base de datos
     try:
-        connection = mysql.connector.connect(**db_config)
-        cursor = connection.cursor(dictionary=True)
+        connection = mysql.connector.connect(**db_config)#Se conecta a la base de datos MySQL usando la configuración definida.
+        cursor = connection.cursor(dictionary=True) # Crea un cursor que devolverá los resultados como diccionarios ({columna: valor}).
 
         sql = """
             SELECT d.id_dispositivo, d.nombre, d.descripcion, 
@@ -30,19 +31,19 @@ def get_dispositivos():
             LEFT JOIN Ubicaciones u ON d.id_dispositivo = u.id_dispositivo
             LEFT JOIN Mediciones m ON d.id_dispositivo = m.id_dispositivo
             GROUP BY d.id_dispositivo, u.latitud, u.longitud;
-        """
+        """ #Consulta 
 
-        cursor.execute(sql)
-        results = cursor.fetchall()
+        cursor.execute(sql) # Ejecuta la consulta en la base de datos.
+        results = cursor.fetchall() # Recupera todos los resultados.
 
-        cursor.close()
+        cursor.close() #Cierra la conexion 
         connection.close()
 
-        return jsonify(results)
+        return jsonify(results) #Convierte los datos en JSON
     
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"error": str(e)}), 500 #Captura de error 
 
 if __name__ == '__main__':
     print("Iniciando la aplicación Flask...")
-    app.run(debug=True, port=3000, host="0.0.0.0")  
+    app.run(debug=True, port=3000, host="0.0.0.0")  #Inicia el servidor
